f25527ec30ea304a939e3d5510608b98464aa7e8
andrejs-is-on-the-computer <63778410+andrejs-is-on-the-computer@users.noreply.github.com>
2023-10-14 19:33:54 +0200

    24751 stars contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,9 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+
+
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -16,9 +19,48 @@ const Contact = () => {
   })
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {}
+    setForm({ ...form, [name]: value });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+
+    //tpuh8s5BRjWdppmiL
+//template_x9e9vdp
+  //service_x9y6o6q
+    emailjs.send(
+      'service_x9y6o6q', 
+      'template_x9e9vdp', 
+      {
+        from_name: form.name,
+        to_name: 'Andrejs',
+        from_email: form.email,
+        to_email: 'andreibic89@gmail.com',
+        message: form.message
+      },
+      'tpuh8s5BRjWdppmiL'
+      )
+      .then(() => {
+        setLoading(false);
+        alert('Thank you. I will get back to you as soon as possible.');
+        
+        setForm({
+          name: '',
+          email: '',
+          message: '',
+        })
+      }, (error) => {
+        setLoading(false);
+        console.log(error);
+        alert('Something went wrong.');
+      })
+      
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
